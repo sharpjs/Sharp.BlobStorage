a0a9d8022d0641359b3215bac78163385aa1be61
Jeff Sharp <jeff.sharp@git>
2018-02-23 13:14:11 -0800

    Add copyright banners.

--- Resolve-Version.ps1
+++ Resolve-Version.ps1
@@ -8,6 +8,20 @@
     The resulting version is published as follows:
     * In Version.props, intended to set assembly and nupkg versions
     * As console output, intended to set the TeamCity build number
+
+    Copyright (C) 2018 Jeffrey Sharp
+
+    Permission to use, copy, modify, and distribute this software for any
+    purpose with or without fee is hereby granted, provided that the above
+    copyright notice and this permission notice appear in all copies.
+
+    THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+    WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+    MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+    ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+    WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+    ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+    OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
 #>
 param (
     # Name of the branch or tag.  Default: "local".
