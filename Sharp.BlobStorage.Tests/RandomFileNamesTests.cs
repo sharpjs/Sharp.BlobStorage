2fceef3f4d31edbc0ffb0a073d0eda4b59e1c91e
Jeff Sharp <jeff.sharp@git>
2018-02-23 13:38:23 -0800

    Add tests project.

---
+++ Sharp.BlobStorage.Tests/RandomFileNamesTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Sharp.BlobStorage.Tests
+{
+    [TestFixture]
+    public class RandomFileNamesTests
+    {
+        [Test]
+        public void SomeTest()
+        {
+            // Stub
+        }
+    }
+}
