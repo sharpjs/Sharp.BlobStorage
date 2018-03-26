c0be50b8c6d6970e7722af10c3f5f5bc84946f01
Jeff Sharp <jeff.sharp@git>
2018-03-26 10:24:47 -0700

    Add extension methods for Uri.

---
+++ Sharp.BlobStorage.Tests/Internal/UriExtensionsTests.cs
@@ -0,0 +1,146 @@
+﻿/*
+    Copyright (C) 2018 Jeffrey Sharp
+
+    Permission to use, copy, modify, and distribute this software for any
+    purpose with or without fee is hereby granted, provided that the above
+    copyright notice and this permission notice appear in all copies.
+
+    THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+    WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+    MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+    ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+    WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+    ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+    OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+*/
+
+using System;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace Sharp.BlobStorage.Internal
+{
+    [TestFixture]
+    public class UriExtensionsTests
+    {
+        private const string
+            Example    = "http://example.com/",
+            ExampleOld = Example + "old/",
+            ExampleNew = Example + "new/",
+            ExampleEtc = Example + "etc/",
+            NotSlashed = "http://user:pass@example.com:1234/foo/bar?a=1&b=2#c",
+            Slashed    = "http://user:pass@example.com:1234/foo/bar/?a=1&b=2#c";
+
+        private static readonly Uri
+            NullUri       = null,
+            AnyUri        = new Uri(Example,              UriKind.Absolute),
+            RelativeUri   = new Uri("relative",           UriKind.Relative),
+            OldBaseUri    = new Uri(ExampleOld,           UriKind.Absolute),
+            OldFileUri    = new Uri(ExampleOld + "a/b.c", UriKind.Absolute),
+            NewBaseUri    = new Uri(ExampleNew,           UriKind.Absolute),
+            NewFileUri    = new Uri(ExampleNew + "a/b.c", UriKind.Absolute),
+            OtherUri      = new Uri(ExampleEtc + "a/b.c", UriKind.Absolute),
+            NotSlashedUri = new Uri(NotSlashed,           UriKind.Absolute),
+            SlashedUri    = new Uri(Slashed,              UriKind.Absolute);
+
+        [Test]
+        public void EnsurePathTrailingSlash_Null()
+        {
+            NullUri
+                .Invoking(u => u.EnsurePathTrailingSlash())
+                .Should().Throw<ArgumentNullException>();
+        }
+
+        [Test]
+        public void EnsurePathTrailingSlash_Relative()
+        {
+            RelativeUri
+                .Invoking(u => u.EnsurePathTrailingSlash())
+                .Should().Throw<ArgumentException>();
+        }
+
+        [Test]
+        public void EnsurePathTrailingSlash_NoSlash()
+        {
+            NotSlashedUri.EnsurePathTrailingSlash()
+                .Should().Be(SlashedUri);
+        }
+
+        [Test]
+        public void EnsurePathTrailingSlash_ExistingSlash()
+        {
+            SlashedUri.EnsurePathTrailingSlash()
+                .Should().Be(SlashedUri);
+        }
+
+        [Test]
+        public void ChangeBase_NullUri()
+        {
+            NullUri
+                .Invoking(u => u.ChangeBase(AnyUri, AnyUri))
+                .Should().Throw<ArgumentNullException>();
+        }
+
+        [Test]
+        public void ChangeBase_NullOldBaseUri()
+        {
+            NullUri
+                .Invoking(u => AnyUri.ChangeBase(u, AnyUri))
+                .Should().Throw<ArgumentNullException>();
+        }
+
+        [Test]
+        public void ChangeBase_NullNewBaseUri()
+        {
+            NullUri
+                .Invoking(u => AnyUri.ChangeBase(AnyUri, u))
+                .Should().Throw<ArgumentNullException>();
+        }
+
+        [Test]
+        public void ChangeBase_RelativeUri()
+        {
+            RelativeUri
+                .Invoking(u => u.ChangeBase(AnyUri, AnyUri))
+                .Should().Throw<ArgumentException>();
+        }
+
+        [Test]
+        public void ChangeBase_RelativeOldBaseUri()
+        {
+            RelativeUri
+                .Invoking(u => AnyUri.ChangeBase(u, AnyUri))
+                .Should().Throw<ArgumentException>();
+        }
+
+        [Test]
+        public void ChangeBase_RelativeNewBaseUri()
+        {
+            RelativeUri
+                .Invoking(u => AnyUri.ChangeBase(AnyUri, u))
+                .Should().Throw<ArgumentException>();
+        }
+
+        [Test]
+        public void ChangeBase_OldBaseMismatch()
+        {
+            OtherUri
+                .Invoking(u => u.ChangeBase(OldBaseUri, NewBaseUri))
+                .Should().Throw<ArgumentException>();
+        }
+
+        [Test]
+        public void ChangeBase_Changed()
+        {
+            OldFileUri.ChangeBase(OldBaseUri, NewBaseUri)
+                .Should().Be(NewFileUri);
+        }
+
+        [Test]
+        public void ChangeBase_Unchanged()
+        {
+            NewFileUri.ChangeBase(NewBaseUri, NewBaseUri)
+                .Should().Be(NewFileUri);
+        }
+    }
+}
---
+++ Sharp.BlobStorage/Internal/UriExtensions.cs
@@ -0,0 +1,130 @@
+﻿using System;
+
+namespace Sharp.BlobStorage.Internal
+{
+    /// <summary>
+    ///   Extension methods for <c>System.Uri</c>.
+    /// </summary>
+    public static class UriExtensions
+    {
+        /// <summary>
+        ///   Ensures that the path component of the current absolute URI ends
+        ///   with a slash.
+        /// </summary>
+        /// <param name="uri">The URI to ensure.</param>
+        /// <returns>
+        ///   <paramref name="uri"/>, if <paramref name="uri"/>'s path
+        ///   component already ends with a slash; otherwise, a copy of
+        ///   <paramref name="uri"/> with a slash appended to the path
+        ///   component.
+        /// </returns>
+        public static Uri EnsurePathTrailingSlash(this Uri uri)
+        {
+            if (uri == null)
+                throw new ArgumentNullException(nameof(uri));
+            if (!uri.IsAbsoluteUri)
+                throw new ArgumentNullException(nameof(uri));
+
+            if (uri.AbsolutePath.EndsWith("/", StringComparison.Ordinal))
+                return uri;
+
+            var builder = new UriBuilder(uri);
+            builder.Path += "/";
+            return builder.Uri;
+        }
+
+        /// <summary>
+        ///   Replaces the specified base URI in the current absolute URI with
+        ///   a new base URI.
+        /// </summary>
+        /// <param name="uri">
+        ///   The absolute URI in which to replace the base URI.
+        /// </param>
+        /// <param name="oldBaseUri">
+        ///   The old base URI to find in <paramref name="uri"/>.
+        /// </param>
+        /// <param name="newBaseUri">
+        ///   The new base URI to insert in <paramref name="uri"/>> in place of
+        ///   <paramref name="oldBaseUri"/>.
+        /// </param>
+        /// <returns>
+        ///   A copy of <paramref name="uri"/> in which
+        ///   <paramref name="oldBaseUri"/> has been replaced with
+        ///   <paramref name="newBaseUri"/>.
+        /// </returns>
+        /// <exception cref="ArgumentNullException">
+        ///   One or more of
+        ///     <paramref name="uri"/>,
+        ///     <paramref name="oldBaseUri"/>, or
+        ///     <paramref name="newBaseUri"/>
+        ///     is <c>null</c>.
+        /// </exception>
+        /// <exception cref="ArgumentException">
+        ///   One or more of
+        ///     <paramref name="uri"/>,
+        ///     <paramref name="oldBaseUri"/>, or
+        ///     <paramref name="newBaseUri"/>
+        ///     is a relative URI; or,
+        ///   <paramref name="oldBaseUri"/> is not a base of
+        ///     <paramref name="uri"/>.
+        /// </exception>
+        public static Uri ChangeBase(this Uri uri, Uri oldBaseUri, Uri newBaseUri)
+        {
+            if (uri == null)
+                throw new ArgumentNullException(nameof(uri));
+            if (oldBaseUri == null)
+                throw new ArgumentNullException(nameof(oldBaseUri));
+            if (newBaseUri == null)
+                throw new ArgumentNullException(nameof(newBaseUri));
+
+            if (!uri.IsAbsoluteUri)
+                throw UriNotAbsoluteError(uri, nameof(uri));
+            if (!oldBaseUri.IsAbsoluteUri)
+                throw UriNotAbsoluteError(oldBaseUri, nameof(oldBaseUri));
+            if (!newBaseUri.IsAbsoluteUri)
+                throw UriNotAbsoluteError(newBaseUri, nameof(newBaseUri));
+
+            if (!oldBaseUri.IsBaseOf(uri))
+                throw UriNotRequiredBaseError(uri, oldBaseUri, nameof(uri));
+
+            var relativeUri = oldBaseUri.MakeRelativeUri(uri);
+            return new Uri(newBaseUri, relativeUri);
+        }
+
+        /// <summary>
+        ///   Creates an <c>ArgumentException</c> appropriate when the
+        ///   specified URI is relative but is required to be absolute.
+        /// </summary>
+        /// <param name="uri">The URI.</param>
+        /// <param name="parameterName">The name of the parameter.</param>
+        /// <returns>An appropriate exception.</returns>
+        public static ArgumentException UriNotAbsoluteError(
+            Uri    uri,
+            string parameterName)
+        {
+            return new ArgumentException(
+                $"The URI '{uri}' is not an absolute URI.",
+                parameterName
+            );
+        }
+
+        /// <summary>
+        ///   Creates an <c>ArgumentException</c> appropriate when the
+        ///   specified URI does not have a required base URI.
+        /// </summary>
+        /// <param name="uri">The URI.</param>
+        /// <param name="baseUri">The required base URI.</param>
+        /// <param name="parameterName">The name of the parameter.</param>
+        /// <returns>An appropriate exception.</returns>
+        public static ArgumentException UriNotRequiredBaseError(
+            Uri    uri,
+            Uri    baseUri,
+            string parameterName)
+        {
+            return new ArgumentException(
+                $"The URI '{uri}' does not have the required base URI '{baseUri}'.",
+                parameterName
+            );
+        }
+    }
+}
